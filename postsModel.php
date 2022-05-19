9484565b6783c87a6151acf7fccd2fcd3c9e0685
DanielStrensHelha <LA217024@student.helha.be>
2022-05-19 12:10:23 +0200

    postsModel.php now insert a sorting parameter in the sql querry

--- postsModel.php
+++ postsModel.php
@@ -4,12 +4,27 @@ if($browsePosts) {
     // get posts from data base
     include('dbConnexion.php');
 
+    $sort = 'AVG(appreciation.likes) DESC';
+    if(!empty($_POST['sort'])) {
+        switch ($_POST['sort']) {
+            case 'name_sort':
+                $sort = 'name_guit ASC';
+                break;
+            case 'most_commented_sort':
+                $sort = "COUNT(comments.id_guitarist) DESC";
+                break;
+        }
+    }
+
     // Get the guitarists
-    $sqlQuerry =    "SELECT thumbnail_guit, guitarist.id_guitarist, name_guit, wiki_hero, AVG(appreciation.likes) AS likes
+    $sqlQuerry =    "SELECT thumbnail_guit, guitarist.id_guitarist, name_guit, wiki_hero, 
+                        AVG(appreciation.likes) AS likes, 
+                        COUNT(comments.id_guitarist) AS comments
                     FROM guitarist
                     LEFT JOIN appreciation ON guitarist.id_guitarist = appreciation.id_guitarist
+                    LEFT JOIN comments ON guitarist.id_guitarist = comments.id_guitarist
                     GROUP BY guitarist.id_guitarist
-                    ORDER BY AVG(appreciation.likes) DESC
+                    ORDER BY $sort
                     LIMIT :startPost, :numberOfPosts;
                     ";
     
